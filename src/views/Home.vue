21662931ccb18e58e8a6238783b253e2ea852a8a
dipadana <putudipadana@gmail.com>
2020-10-01 16:36:11 +0800

    add several component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>m3u8 Downloader</h1>
+  <div class="bg-gray-100">
+    <div class="container m-auto">
+      <div class="flex flex-col justify-center items-center h-screen">
+        <h1 class="text-3xl text-green-500 font-semibold">.m3u8 Downloader</h1>
+        <div class="my-4 w-full flex justify-center">
+          <input class="border border-grey-300 focus:border-green-500 border-solid outline-none w-6/12 py-2 px-3 rounded" placeholder="Please input .m3u8 link here" type="text">
+        </div>
+        <button class="border border-green-500 py-2 px-3 bg-green-500 text-white rounded">
+          <i class="fas fa-file-download mr-2"></i>
+          Download
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,7 +26,7 @@ export default defineComponent({
     const router = useRouter()
 
     console.log(route.fullPath)
-    
+
   }
 });
 </script>
